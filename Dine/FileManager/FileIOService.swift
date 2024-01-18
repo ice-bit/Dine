3cacd5386e267b05727192f4673b41072cd82bd3
Goutham_Das <gouthamdas.t@zohocorp.com>
2024-01-18 14:12:15 +0530

    Added util classes for File IO and JSON Serialiazation.

---
+++ Dine/FileManager/FileIOService.swift
@@ -0,0 +1,81 @@
+//
+//  FileIOController.swift
+//  Dine
+//
+//  Created by doss-zstch1212 on 12/01/24.
+//
+
+import Foundation
+
+/// Enumeration representing possible errors related to file input and output operations.
+/// - `permissionDenied`: The operation failed due to insufficient permissions.
+/// - `fileNotFound`: The specified file could not be found.
+/// - `fileAlreadyExists`: Attempted to create a file that already exists.
+/// - `invalidFileURL`: The provided file URL is considered invalid.
+/// - `encodingError`: An error occurred while encoding data.
+/// - `decodingError`: An error occurred while decoding data.
+/// - `documentDirectoryUnavailable`: The document directory, a common location for file operations,
+///    is not available.
+enum FileIOError: Error {
+    case permissionDenied
+    case fileNotFound
+    case fileAlreadyExists
+    case invalidFileURL
+    case encodingError
+    case decodingError
+    case documentDirectoryUnavailable
+}
+
+class FileIOService {
+    
+    func write(data: Data, into fileName: String) throws {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            throw FileIOError.documentDirectoryUnavailable
+        }
+        
+        let fileURL = documentDirectory.appending(path: fileName)
+        
+        do {
+            try data.write(to: fileURL)
+            print("Data written to \(fileName) successfully!")
+        } catch {
+            print("Error writing JSON to file: \(error)")
+        }
+    }
+    
+    func read<T: Codable>(from fileName: String) throws -> T {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            throw FileIOError.documentDirectoryUnavailable
+        }
+        
+        let fileURL = documentDirectory.appending(path: fileName)
+        
+        do {
+            let savedJSONData = try Data(contentsOf: fileURL)
+            let modal = try JSONDecoder().decode(T.self, from: savedJSONData)
+            return modal
+        } catch {
+            print("Error decoding JSON: \(error)")
+            throw FileIOError.decodingError
+        }
+    }
+    
+    private func handleFileError(_ error: FileIOError) {
+        switch error {
+        case .permissionDenied:
+            print("Permission denied. Unable to perform file operation.")
+        case .fileNotFound:
+            print("File not found.")
+        case .fileAlreadyExists:
+            print("File with the same name already exists.")
+        case .invalidFileURL:
+            print("Invalid file URL.")
+        case .encodingError:
+            print("Error encoding file content.")
+        case .decodingError:
+            print("Error decoding file content.")
+        case .documentDirectoryUnavailable:
+            print("Unable to access the document directory.")
+        }
+    }
+}
---
+++ Dine/Utilities/ModelJSONSerializer.swift
@@ -0,0 +1,8 @@
+//
+//  ModelJSONSerializer.swift
+//  Dine
+//
+//  Created by doss-zstch1212 on 18/01/24.
+//
+
+import Foundation
