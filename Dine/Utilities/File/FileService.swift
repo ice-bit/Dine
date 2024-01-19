1048ef6c0a10d8d5736454a15ac0c9338b9ca618
Goutham_Das <gouthamdas.t@zohocorp.com>
2024-01-19 19:48:45 +0530

    Updated `FileService` methods for saving modal.

--- Dine/Utilities/File/FileService.swift
+++ Dine/Utilities/File/FileService.swift
@@ -24,16 +24,51 @@ class FileService {
         return FileManager.default.fileExists(atPath: fileURL.path)
     }
     
-    func saveModal<T: Codable>(_ modal: T, fileName: String, completion: @escaping (Result<Void, Error>) -> Void) {
+    /// Saves a Codable modal object to a JSON file asynchronously.
+    /// - Parameters:
+    ///   - modal: The Codable modal object to be saved.
+    ///   - fileName: The name of the file (without extension) where the modal object will be saved.
+    ///   - completion: A completion handler that is called after the write operation is completed. It provides a Result enum with a success case (`Void`) on successful write and a failure case (`FileIOError`) if an error occurs during the process.
+    func saveModal<T: Codable>(_ modal: T, fileName: String, completion: @escaping (Result<Void, FileIOError>) -> Void) {
+        let fileIO = FileIOService()
+        let modalJSONSerializer = ModelJSONSerializer()
         do {
-            let fileIO = FileIOService()
-            let modalJSONSerializer = ModelJSONSerializer()
             let data = try modalJSONSerializer.modelToJSON(model: modal)
-            try fileIO.write(data: data, into: "\(fileName).json")
-            completion(.success(()))
+            fileIO.write(data: data, into: "\(fileName).json") { result in
+                switch result {
+                case .success:
+                    print("Successfully written to file: \(fileName)")
+                    completion(.success(()))
+                case .failure(let error):
+                    print("Failed to perform write: \(error)")
+                    completion(.failure(error))
+                }
+            }
         } catch {
-            completion(.failure(error))
+            completion(.failure(.encodingError))
         }
     }
-
+    
+    /// Saves a Codable modal object to a JSON file synchronously.
+    /// - Parameters:
+    ///   - modal: The Codable modal object to be saved.
+    ///   - fileName: The name of the file (without extension) where the modal object will be
+    /// - Throws: A `FileIOError.encodingError` if an error occurs during the encoding or writing process.
+    func saveModal<T: Codable>(_ modal: T, fileName: String) throws {
+        let fileIO = FileIOService()
+        let modalJSONSerializer = ModelJSONSerializer()
+        do {
+            let data = try modalJSONSerializer.modelToJSON(model: modal)
+            fileIO.write(data: data, into: "\(fileName).json") { result in
+                switch result {
+                case .success:
+                    print("Successfully written to file: \(fileName)")
+                case .failure(let error):
+                    print("Failed to perform write: \(error)")
+                }
+            }
+        } catch {
+            throw FileIOError.encodingError
+        }
+    }
 }
