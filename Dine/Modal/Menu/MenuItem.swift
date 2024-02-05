6fa7948fb92584496e32fe6b992968272d6525fa
Goutham_Das <gouthamdas.t@zohocorp.com>
2024-02-05 17:13:46 +0530

    Added UUID.

--- Dine/Modal/Menu/MenuItem.swift
+++ Dine/Modal/Menu/MenuItem.swift
@@ -8,27 +8,12 @@
 import Foundation
 
 class MenuItem: Codable {
-    /*private var menuItemId: UUID
-    private var title: String
-    private var description: String
-    private var price: Double
-    
-    init(menuItemId: UUID, title: String, description: String, price: Double) {
-        self.menuItemId = menuItemId
-        self.title = title
-        self.description = description
-        self.price = price
-    }
-    
-    func updatePrice(title: String, description: String) {
-        
-    }*/
-
-    let itemId = UUID()
+    let itemId: UUID
     let name: String
     let price: Double
     
     init(name: String, price: Double) {
+        self.itemId = UUID()
         self.name = name
         self.price = price
     }
